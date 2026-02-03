065ebdcc8fce9afb1a5bd1e9e8b073cb3d2211ce
manthan2305 <manthansolanki1999@gmail.com>
2026-02-04 00:36:02 +0100

    prompt updated

--- examples/wanvideo/model_training/validate_lora/Wan2.1-VACE-1.3B.py
+++ examples/wanvideo/model_training/validate_lora/Wan2.1-VACE-1.3B.py
@@ -27,11 +27,12 @@ ref_img = VideoData("data_infer/other_1.jpg", height=480, width=832)[0]
 #vace_reference_video=[ref_img, ref_img_1, ref_img_2, ref_img_3, ref_img_4]
 vace_reference_video=ref_img
 
-prompt=("Man is dancing by following the video pose exactly. Dance is natural and smooth.",
-        " Maintain the exact facial features, hair, clothing and background from the reference image.",
-        "Keep the background consistent with the reference image.",
-	"Man is bearing blue pants, white shoes and black t-shirt."
-)
+prompt="""
+Man is dancing by following the video pose exactly. Dance is natural and smooth.
+Maintain the exact facial features, hair, clothing and background from the reference image.
+Keep the background consistent with the reference image.
+Man is bearing blue pants, white shoes and black t-shirt.
+"""
 video = pipe(
     prompt=prompt,
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
